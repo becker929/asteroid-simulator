229941693d57fc011341f5d066c922a1f1dc0894
Anthony Becker <anthonybecker9@gmail.com>
2024-04-24 19:38:38 -0700

    initial commit

---
+++ game.py
@@ -0,0 +1,7 @@
+# To start with, there will be a few objects in the game:
+# - an asteroid
+# - a mining mission
+# - an outcome
+# The core game loop is that the player will be told about an asteroid
+# They will then be able to outfit and launch a mining mission
+# Then they will get an outcome
